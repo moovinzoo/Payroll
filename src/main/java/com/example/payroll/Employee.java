edf4b532b4e8a6c949d6abf654d8b6a82a02bd38
DJ Lee <moovinzoo@gmail.com>
2023-06-13 22:15:26 +0900

    init

---
+++ src/main/java/com/example/payroll/Employee.java
@@ -0,0 +1,26 @@
+package com.example.payroll;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
