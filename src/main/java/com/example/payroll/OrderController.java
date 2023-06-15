83d20c4db49f4803bd8d7ce4c1be794e1c3c601b
DJ Lee <moovinzoo@gmail.com>
2023-06-15 21:00:09 +0900

    add: newOrder() method under request POST "/orders"

--- src/main/java/com/example/payroll/OrderController.java
+++ src/main/java/com/example/payroll/OrderController.java
@@ -3,8 +3,12 @@ package com.example.payroll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -39,4 +43,29 @@ public class OrderController {
 
         return assembler.toModel(order);
     }
+
+    @PostMapping("/orders")
+    public ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
+
+        order.setStatus(Status.IN_PROGRESS);
+        Order newOrder = repository.save(order);
+
+        EntityModel<Order> entityModel = assembler.toModel(newOrder);
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+
+    /*
+      @PostMapping("/orders")
+  ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
+
+    order.setStatus(Status.IN_PROGRESS);
+    Order newOrder = orderRepository.save(order);
+
+    return ResponseEntity //
+        .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri()) //
+        .body(assembler.toModel(newOrder));
+  }
+     */
 }
