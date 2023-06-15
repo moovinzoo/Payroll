1b0e2d4bb27019aa8f0632001c942733e0dfb2ff
DJ Lee <moovinzoo@gmail.com>
2023-06-15 15:39:59 +0900

    add: inject assembler to simplify model-transition code

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -23,31 +23,32 @@ public class EmployeeController {
 
     private final EmployeeRepository repository;
 
+    private final EmployeeModelAssembler assembler;
+
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
+
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .map(assembler::toModel)
                 .collect(Collectors.toList());
+
         return CollectionModel.of(employees,
                 linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
+
         return repository.save(newEmployee);
     }
-    
 
     @GetMapping("/employees/{id}")
     EntityModel<Employee> one(@PathVariable long id) {
+
         Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
