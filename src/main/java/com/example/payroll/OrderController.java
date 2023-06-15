d5dd08144a43ec55dbf6dc602249c3c8acdb04c8
DJ Lee <moovinzoo@gmail.com>
2023-06-15 20:32:51 +0900

    add: all() method under request GET "/orders"ˆ

---
+++ src/main/java/com/example/payroll/OrderController.java
@@ -0,0 +1,32 @@
+package com.example.payroll;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderRepository repository;
+
+    private final OrderModelAssembler assembler;
+
+    @GetMapping("/orders")
+    public CollectionModel<EntityModel<Order>> all() {
+
+        List<EntityModel<Order>> orders = repository.findAll().stream()
+                .map(assembler::toModel)
+                .toList();
+
+        return CollectionModel.of(orders,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
+}
