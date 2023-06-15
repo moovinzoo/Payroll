9065dd2db7757d8a79348937d3fe2de964f36248
DJ Lee <moovinzoo@gmail.com>
2023-06-15 20:41:30 +0900

    add: one() method under request GET "/orders/{id}"
    - Add not-found-exception class for order

--- src/main/java/com/example/payroll/OrderController.java
+++ src/main/java/com/example/payroll/OrderController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -28,5 +30,14 @@ public class OrderController {
 
         return CollectionModel.of(orders,
                 linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/orders/{id}")
+    public EntityModel<Order> one(@PathVariable Long id) {
+
+        Order order = repository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        return assembler.toModel(order);
     }
 }
