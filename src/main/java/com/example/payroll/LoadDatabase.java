d011af31a9627d51b5b88468ba44b06b0f127bb3
DJ Lee <moovinzoo@gmail.com>
2023-06-15 23:43:53 +0900

    add: update LoadDatabase to pre-load some Orders along with the Employee

--- src/main/java/com/example/payroll/LoadDatabase.java
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -10,11 +10,21 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
 
     @Bean
-    public CommandLineRunner initDatabase(EmployeeRepository repository) {
+    public CommandLineRunner initDatabase(EmployeeRepository employeeRepository,
+                                          OrderRepository orderRepository) {
 
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("DJ", "Lee", "Developer")));
-            log.info("Preloading " + repository.save(new Employee("JH", "Lee", "Doctor")));
+            // Preload employees
+            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            // Preload orders
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preloaded " + order));
         };
     }
 }
